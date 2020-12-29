fdf7eb4c96a86d4418b79fc6ff80cbc80ca2dd8a
QMCHE <39144276+QMCHE@users.noreply.github.com>
2020-12-29 14:22:35 +0900

    Add comment for each function

--- controllers/diary.go
+++ controllers/diary.go
@@ -1,13 +1,17 @@
 package controllers
 
 import (
+	"log"
 	"net/http"
+	"strconv"
 	"strings"
 
+	"github.com/QMCHE/diary-server/models"
 	"github.com/gin-gonic/gin"
 )
 
-func createDiary(c *gin.Context) {
+// CreateDiary creates diary
+func CreateDiary(c *gin.Context) {
 	title := c.PostForm("title")
 	content := c.PostForm("content")
 
@@ -15,5 +19,53 @@ func createDiary(c *gin.Context) {
 		c.JSON(http.StatusBadRequest, gin.H{
 			"error": "Parameter(s) can't be empty",
 		})
+		return
 	}
+
+	err := models.InsertDiary(title, content)
+	if err != nil {
+		log.Print(err)
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": "Create diary failed",
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Diary has been created",
+	})
+	return
+}
+
+// UpdateDiary updates diary
+func UpdateDiary(c *gin.Context) {
+	id, _ := strconv.Atoi(c.Param("id"))
+	title := c.PostForm("title")
+	content := c.PostForm("content")
+
+	err := models.UpdateDiary(id, title, content)
+	if err != nil {
+		log.Print(err)
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": "Update diary failed",
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Diary has been updated",
+	})
+	return
+}
+
+// DeleteDiary deletes diary by id
+func DeleteDiary(c *gin.Context) {
+	id, _ := strconv.Atoi(c.Param("id"))
+	log.Print(id)
+
+	err := models.DeleteDiary(id)
+	if err != nil {
+		log.Print(err)
+	}
+
 }
--- main.go
+++ main.go
@@ -11,5 +11,9 @@ func main() {
 	r.POST("/login", controllers.Login)
 	r.POST("/register", controllers.Register)
 
+	r.POST("/create", controllers.CreateDiary)
+	r.PUT("/update/:id", controllers.UpdateDiary)
+	r.DELETE("/delete/:id", controllers.DeleteDiary)
+
 	r.Run()
 }
