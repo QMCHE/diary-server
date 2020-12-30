bf2e4f281537c9626fd84b4dbb6650ae6de42139
QMCHE <39144276+QMCHE@users.noreply.github.com>
2020-12-30 14:08:05 +0900

    Test code for user.go

--- models/user_test.go
+++ models/user_test.go
@@ -1,28 +1,36 @@
 package models_test
 
 import (
+	"log"
 	"testing"
 
 	"github.com/QMCHE/diary-server/models"
+	"github.com/QMCHE/diary-server/utils"
 )
 
 func TestIsUserExists(t *testing.T) {
-	user := models.IsUserExists("test", "1234")
-	if user {
-		t.Error("User not exist")
+	db := utils.DBConnect()
+
+	err := models.IsUserExists(db, "test", "1234")
+	if err != nil {
+		log.Print(err)
+		t.Error(err)
 	}
 }
 
 func TestIsUniqueUserID(t *testing.T) {
-	isUnique := models.IsUniqueUserID("test")
-	if !isUnique {
-		t.Error("UserID is not unique")
+	db := utils.DBConnect()
+
+	if !models.IsUniqueUserID(db, "test") {
+		t.Error("'test' is not unique user id")
 	}
 }
 
 func TestInsertUser(t *testing.T) {
-	result := models.InsertUser("test", "test1", "1234")
-	if result != nil {
-		t.Error("Failed to insert user")
+	db := utils.DBConnect()
+
+	err := models.InsertUser(db, "test", "test", "1234")
+	if err != nil {
+		t.Error(err)
 	}
 }
