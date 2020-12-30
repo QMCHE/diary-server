7a51d8e92d25e0c58a9915e30e23dcea5aecde55
QMCHE <39144276+QMCHE@users.noreply.github.com>
2020-12-30 14:00:58 +0900

    Creates the functions required for login and register

--- models/user.go
+++ models/user.go
@@ -16,6 +16,25 @@ type User struct {
 	Diaries  []Diary
 }
 
+// IsUserExists checks is user exists
+func IsUserExists(db *gorm.DB, userID, password string) error {
+	var id string
+	encryptedPassword := encryptPassword(password)
+	return db.Raw("SELECT id FROM user WHERE userId=? AND password=?", userID, encryptedPassword).Row().Scan(&id)
+}
+
+// IsUniqueUserID checks if the userId is unique
+func IsUniqueUserID(db *gorm.DB, userID string) bool {
+	var id string
+	return db.Raw("SELECT id FROM user WHERE userId=?", userID).Row().Scan(&id) != nil
+}
+
+// InsertUser inserts user to db
+func InsertUser(db *gorm.DB, name, userID, password string) error {
+	encryptedPassword := encryptPassword(password)
+	return db.Exec("INSERT INTO user (name, userId, password) VALUES (?, ?, ?)", name, userID, encryptedPassword).Error
+}
+
 func encryptPassword(password string) string {
 	sha := sha512.New()
 	sha.Write([]byte(password))
