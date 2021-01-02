b54cf44b7120d1ab54c7a7663592adaabf7ecf37
QMCHE <39144276+QMCHE@users.noreply.github.com>
2021-01-02 17:22:46 +0900

    Migration error handling
    Move the .env file to main.go
    Add parameters to destination
    Migration error handling

--- utils/mysql.go
+++ utils/mysql.go
@@ -4,33 +4,29 @@ import (
 	"os"
 
 	"github.com/QMCHE/diary-server/models"
-	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 // DBConnect returns db object
 func DBConnect() *gorm.DB {
-	err := godotenv.Load()
-	if err != nil {
-		panic(err)
-	}
-
 	id := os.Getenv("ID")
 	password := os.Getenv("PASSWORD")
 	protocol := os.Getenv("PROTOCOL")
-	DBAddress := os.Getenv("DBADDRESS")
-	DBPort := os.Getenv("DBPORT")
-	DBName := os.Getenv("DBNAME")
+	DBAddress := os.Getenv("DB_ADDRESS")
+	DBPort := os.Getenv("DB_PORT")
+	DBName := os.Getenv("DB_NAME")
 
-	dsn := id + ":" + password + "@" + protocol + "(" + DBAddress + ":" + DBPort + ")/" + DBName
+	dsn := id + ":" + password + "@" + protocol + "(" + DBAddress + ":" + DBPort + ")/" + DBName + "?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-
 	if err != nil {
 		panic(err)
 	}
 
-	db.AutoMigrate(&models.User{}, &models.Diary{})
+	err = db.AutoMigrate(&models.User{}, &models.Diary{})
+	if err != nil {
+		panic(err)
+	}
 
 	return db
 }
