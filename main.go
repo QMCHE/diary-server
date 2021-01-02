e7cdca92597be504c5176e89cddf8c3008b8ee36
QMCHE <39144276+QMCHE@users.noreply.github.com>
2021-01-02 17:18:48 +0900

    Load .env file

--- main.go
+++ main.go
@@ -3,9 +3,15 @@ package main
 import (
 	"github.com/QMCHE/diary-server/controllers"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		panic(err)
+	}
+
 	gin.SetMode(gin.DebugMode)
 	r := gin.Default()
 
