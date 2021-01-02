049b53eba2443cb6bda387277f739ef6d0121bd9
QMCHE <39144276+QMCHE@users.noreply.github.com>
2021-01-02 13:38:03 +0900

    Delete sensitive informations

--- utils/mysql.go
+++ utils/mysql.go
@@ -1,18 +1,36 @@
 package utils
 
 import (
+	"os"
+
+	"github.com/QMCHE/diary-server/models"
+	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 // DBConnect returns db object
 func DBConnect() *gorm.DB {
-	dsn := "root:1234@tcp(127.0.0.1:3306)/diary"
+	err := godotenv.Load()
+	if err != nil {
+		panic(err)
+	}
+
+	id := os.Getenv("ID")
+	password := os.Getenv("PASSWORD")
+	protocol := os.Getenv("PROTOCOL")
+	DBAddress := os.Getenv("DBADDRESS")
+	DBPort := os.Getenv("DBPORT")
+	DBName := os.Getenv("DBNAME")
+
+	dsn := id + ":" + password + "@" + protocol + "(" + DBAddress + ":" + DBPort + ")/" + DBName
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
 		panic(err)
 	}
+
+	db.AutoMigrate(&models.User{}, &models.Diary{})
 
 	return db
 }
