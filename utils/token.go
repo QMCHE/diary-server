b1c4051d47c8d03688f40419f095ded35f8b6ac6
QMCHE <39144276+QMCHE@users.noreply.github.com>
2021-01-08 15:40:53 +0900

    Fix time

--- utils/token.go
+++ utils/token.go
@@ -18,12 +18,12 @@ type Claims struct {
 
 // GenerateAccessToken returns access token
 func GenerateAccessToken(user *models.User) (string, error) {
-	expirationTime := 5 * time.Minute
+	expirationTime := time.Now().Add(5 * time.Minute).Unix()
 	claims := &Claims{
 		UserID:  user.UserID,
 		IsAdmin: user.IsAdmin,
 		StandardClaims: jwt.StandardClaims{
-			ExpiresAt: expirationTime.Milliseconds(),
+			ExpiresAt: expirationTime,
 		},
 	}
 	return generateToken(claims)
@@ -31,33 +31,36 @@ func GenerateAccessToken(user *models.User) (string, error) {
 
 // GenerateRefreshToken returns refresh token
 func GenerateRefreshToken(user *models.User) (string, error) {
-	expirationTime := 24 * time.Hour * 7
+	expirationTime := time.Now().Add(24 * time.Hour * 7).Unix()
 	claims := &Claims{
 		IsAdmin: user.IsAdmin,
 		StandardClaims: jwt.StandardClaims{
 			Issuer:    "localhost",
-			ExpiresAt: expirationTime.Milliseconds(),
+			ExpiresAt: expirationTime,
 		},
 	}
 	return generateToken(claims)
 }
 
 // VerifyToken verifies that the token is valid
-func VerifyToken(token string) (*jwt.Token, error) {
-	claims, err := jwt.Parse(token, func(t *jwt.Token) (interface{}, error) {
+func VerifyToken(tokenString string) (*Claims, error) {
+	token, err := jwt.ParseWithClaims(tokenString, &Claims{}, func(t *jwt.Token) (interface{}, error) {
 		if _, isValid := t.Method.(*jwt.SigningMethodHMAC); !isValid {
 			return nil, errors.New("Unexpected signing method")
 		}
 		return []byte(os.Getenv("JWT_KEY")), nil
 	})
-	if err != nil {
-		return nil, err
+
+	if claims, ok := token.Claims.(*Claims); ok && token.Valid {
+		return claims, nil
 	}
-	return claims, nil
+
+	return nil, err
 }
 
 func generateToken(claims *Claims) (string, error) {
 	key := []byte(os.Getenv("JWT_KEY"))
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 	return token.SignedString(key)
+
 }
