d67a5932f3ed4178632bfeb9a987d953953c2e53
QMCHE <39144276+QMCHE@users.noreply.github.com>
2021-01-11 17:18:42 +0900

    Fix relationship settings between models and tags

--- models/diary.go
+++ models/diary.go
@@ -1,21 +1,27 @@
 package models
 
 import (
+	"time"
+
 	"gorm.io/gorm"
 )
 
 // Diary is struct of diary
 type Diary struct {
-	gorm.Model
-	Title   string `gorm:"size:45;NOT NULL;" json:"title"`
-	Content string `gorm:"size:1000;NOT NULL;" json:"content"`
-	Author  string `gorm:"foreignKey:UserID;references:Author" json:"author"`
+	ID        uint            `gorm:"primarykey" json:"id"`
+	CreatedAt time.Time       `json:"created_at"`
+	UpdatedAt time.Time       `json:"updated_at"`
+	DeletedAt *gorm.DeletedAt `gorm:"index" json:"deleted_at,omitempty"`
+	Title     string          `gorm:"size:45;NOT NULL;" json:"title"`
+	Content   string          `gorm:"size:1000;NOT NULL;" json:"content"`
+	UserID    uint            `json:"userId"`
+	User      User            `gorm:"foreignKey:UserID" json:"user"`
 }
 
 // GetDiary returns diaries
 func GetDiary(db *gorm.DB, sort, direction string, perPage, page int) ([]Diary, error) {
 	var diaries []Diary
-	err := db.Model(&Diary{}).Order(sort + " " + direction).Limit(perPage).Offset(perPage * (page - 1)).Find(&diaries).Error
+	err := db.Model(&Diary{}).Order(sort + " " + direction).Limit(perPage).Offset(perPage * (page - 1)).Preload("User").Find(&diaries).Error
 	if err != nil {
 		return nil, err
 	}
--- models/user.go
+++ models/user.go
@@ -3,24 +3,26 @@ package models
 import (
 	"crypto/sha512"
 	"encoding/hex"
-	"log"
+	"time"
 
 	"gorm.io/gorm"
 )
 
 // User is struct of user
 type User struct {
-	gorm.Model
-	Name     string  `gorm:"size:45;NOT NULL;" json:"name"`
-	UserID   string  `gorm:"size:45;NOT NULL;UNIQUE;" json:"userId"`
-	Password string  `gorm:"size:1000;NOT NULL;" json:"password"`
-	Diaries  []Diary `gorm:"foreignKey:ID;references:ID"`
+	ID        uint            `gorm:"primarykey" json:"id"`
+	CreatedAt time.Time       `json:"created_at"`
+	UpdatedAt time.Time       `json:"updated_at"`
+	DeletedAt *gorm.DeletedAt `gorm:"index" json:"deleted_at,omitempty"`
+	Name      string          `gorm:"size:45;NOT NULL;" json:"name"`
+	UserID    string          `gorm:"size:45;NOT NULL;UNIQUE;" json:"userId"`
+	Password  string          `gorm:"size:1000;NOT NULL;" json:"-"`
+	IsAdmin   bool            `gorm:"default:false;NOT NULL;" json:"isAdmin"`
 }
 
 // IsUserExists checks is user exists
 func (u *User) IsUserExists(db *gorm.DB) bool {
 	encryptedPassword := encryptPassword(u.Password)
-	log.Print(db.Model(User{}).Where("user_id = ? AND password = ?", u.UserID, encryptedPassword).Take(&u).Error)
 	return db.Model(User{}).Where("user_id = ? AND password = ?", u.UserID, encryptedPassword).Take(&u).Error == nil
 }
 
@@ -34,6 +36,10 @@ func (u *User) CreateUser(db *gorm.DB) error {
 	u.Password = encryptPassword(u.Password)
 	return db.Create(&u).Error
 }
+
+// func GetUserByUserID(db *gorm.DB, userID string) {
+// 	db.Model(&User{}).Where("user_id = ?", userID)
+// }
 
 func encryptPassword(password string) string {
 	sha := sha512.New()
