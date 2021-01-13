d312c3e44f252a74d2c70923f5d149bcae86d33b
QMCHE <39144276+QMCHE@users.noreply.github.com>
2021-01-13 18:41:22 +0900

    Update response format

--- controllers/auth.go
+++ controllers/auth.go
@@ -3,7 +3,6 @@ package controllers
 import (
 	"net/http"
 	"strings"
-	"time"
 
 	"github.com/QMCHE/diary-server/models"
 	"github.com/QMCHE/diary-server/utils"
@@ -17,12 +16,18 @@ func Login(c *gin.Context) {
 
 	if strings.Trim(userID, " ") == "" || strings.Trim(password, " ") == "" {
 		c.JSON(http.StatusBadRequest, gin.H{
-			"message": "Parameter(s) can't be empty",
+			"error": "Parameter(s) can't be empty",
 		})
 		return
 	}
 
-	db := utils.DBConnect()
+	db, err := utils.DBConnect()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": "Failed to connect to database",
+		})
+	}
+
 	user := &models.User{
 		UserID:   userID,
 		Password: password,
@@ -30,31 +35,30 @@ func Login(c *gin.Context) {
 
 	if !user.IsUserExists(db) {
 		c.JSON(http.StatusUnauthorized, gin.H{
-			"message": "User not exists",
+			"error": "User not exists",
 		})
 		return
 	}
 
-	accessToken, err := utils.GenerateAccessToken(user.ID, user.UserID)
+	accessToken, err := utils.GenerateAccessToken(user)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{
-			"message": "Generate token failed",
+			"error": "Generate token failed",
 		})
 		return
 	}
 
-	refreshToken, err := utils.GenerateRefreshToken(user.ID, user.UserID)
+	refreshToken, err := utils.GenerateRefreshToken(user)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{
-			"message": "Generate token failed",
+			"error": "Generate token failed",
 		})
 		return
 	}
 
-	c.SetCookie("refreshToken", refreshToken, int(24*time.Hour*7), "/", "/", true, true)
 	c.JSON(http.StatusOK, gin.H{
-		"message": "Login success",
-		"token":   accessToken,
+		"access-token":  accessToken,
+		"refresh-token": refreshToken,
 	})
 	return
 }
@@ -67,12 +71,18 @@ func Register(c *gin.Context) {
 
 	if strings.Trim(name, " ") == "" || strings.Trim(userID, " ") == "" || strings.Trim(password, " ") == "" {
 		c.JSON(http.StatusBadRequest, gin.H{
-			"message": "Parameter(s) can't be empty",
+			"error": "Parameter(s) can't be empty",
 		})
 		return
 	}
 
-	db := utils.DBConnect()
+	db, err := utils.DBConnect()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": "Failed to connect to database",
+		})
+	}
+
 	user := &models.User{
 		Name:     name,
 		UserID:   userID,
@@ -81,21 +91,87 @@ func Register(c *gin.Context) {
 
 	if !user.IsUniqueUserID(db) {
 		c.JSON(http.StatusBadRequest, gin.H{
-			"message": "Duplicate userId",
+			"error": "Duplicate userId",
 		})
 		return
 	}
 
-	err := user.CreateUser(db)
+	err = user.CreateUser(db)
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{
-			"message": "Insert user failed",
+			"error": "Insert user failed",
 		})
 		return
 	}
 
 	c.JSON(http.StatusOK, gin.H{
-		"message": "Register success",
+		"user": user,
+	})
+	return
+}
+
+// ReissueAccessToken reissue the token
+func ReissueAccessToken(c *gin.Context) {
+	refreshToken := c.Request.Header.Get("refresh-token")
+	accessToken := c.Request.Header.Get("access-token")
+	if strings.Trim(refreshToken, " ") == "" {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Refresh token is required",
+		})
+		return
+	}
+
+	if strings.Trim(accessToken, " ") == "" {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Access token is required",
+		})
+		return
+	}
+
+	if utils.IsExpired(refreshToken) {
+		c.JSON(http.StatusUnauthorized, gin.H{
+			"error": "Refresh token is expired",
+		})
+		return
+	}
+
+	claims, err := utils.VerifyToken(refreshToken)
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{
+			"error": "Invalid refresh token",
+		})
+		return
+	}
+
+	db, err := utils.DBConnect()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": "Failed to connect database",
+		})
+		return
+	}
+	user := &models.User{
+		ID: claims.ID,
+	}
+
+	err = user.GetUserByID(db)
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{
+			"error": "Can't find user",
+		})
+		return
+	}
+
+	newAccessToken, err := utils.GenerateAccessToken(user)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": "Failed to generate token",
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"access-token": newAccessToken,
 	})
 	return
 }
--- controllers/diary.go
+++ controllers/diary.go
@@ -1,17 +1,65 @@
 package controllers
 
 import (
-	"log"
 	"net/http"
 	"strconv"
 	"strings"
 
 	"github.com/QMCHE/diary-server/models"
+	"github.com/QMCHE/diary-server/utils"
 	"github.com/gin-gonic/gin"
 )
 
-// CreateDiary creates diary
+// GetDiary is getting all diary controller
+func GetDiary(c *gin.Context) {
+	sort := c.PostForm("sort")
+	direction := c.PostForm("direction")
+	perPage, _ := strconv.Atoi(c.PostForm("per_page"))
+	page, _ := strconv.Atoi(c.PostForm("page"))
+
+	if strings.Trim(sort, " ") == "" {
+		sort = "created_at"
+	}
+	if strings.Trim(direction, " ") == "" {
+		direction = "desc"
+	}
+	if perPage <= 0 {
+		perPage = 50
+	}
+	if page <= 0 {
+		page = 1
+	}
+
+	db, err := utils.DBConnect()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": "Failed to connect to database",
+		})
+	}
+
+	diaries, err := models.GetDiary(db, sort, direction, perPage, page)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": "Get diaries failed",
+		})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"diaries": diaries,
+	})
+	return
+}
+
+// CreateDiary is creating diary controller
 func CreateDiary(c *gin.Context) {
+	claims, isExists := c.Get("claims")
+	if !isExists {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": "Can't get user information",
+		})
+		return
+	}
+
 	title := c.PostForm("title")
 	content := c.PostForm("content")
 
@@ -22,50 +70,29 @@ func CreateDiary(c *gin.Context) {
 		return
 	}
 
-	err := models.InsertDiary(title, content)
+	db, err := utils.DBConnect()
 	if err != nil {
-		log.Print(err)
 		c.JSON(http.StatusInternalServerError, gin.H{
-			"error": "Create diary failed",
+			"error": "Failed to connect to database",
+		})
+	}
+
+	diary := &models.Diary{
+		Title:   title,
+		Content: content,
+		UserID:  claims.(*utils.Claims).ID,
+	}
+
+	err = diary.InsertDiary(db)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": "Diary inserts failed",
 		})
 		return
 	}
 
 	c.JSON(http.StatusOK, gin.H{
-		"message": "Diary has been created",
+		"diary": diary,
 	})
 	return
-}
-
-// UpdateDiary updates diary
-func UpdateDiary(c *gin.Context) {
-	id, _ := strconv.Atoi(c.Param("id"))
-	title := c.PostForm("title")
-	content := c.PostForm("content")
-
-	err := models.UpdateDiary(id, title, content)
-	if err != nil {
-		log.Print(err)
-		c.JSON(http.StatusInternalServerError, gin.H{
-			"error": "Update diary failed",
-		})
-		return
-	}
-
-	c.JSON(http.StatusOK, gin.H{
-		"message": "Diary has been updated",
-	})
-	return
-}
-
-// DeleteDiary deletes diary by id
-func DeleteDiary(c *gin.Context) {
-	id, _ := strconv.Atoi(c.Param("id"))
-	log.Print(id)
-
-	err := models.DeleteDiary(id)
-	if err != nil {
-		log.Print(err)
-	}
-
 }
