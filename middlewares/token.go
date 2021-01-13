535f40d0e5466fd959b5850710d07bb013e9e7ab
QMCHE <39144276+QMCHE@users.noreply.github.com>
2021-01-13 18:41:46 +0900

    Add token validation middleware

---
+++ middlewares/token.go
@@ -0,0 +1,45 @@
+package middlewares
+
+import (
+	"log"
+	"net/http"
+	"strings"
+
+	"github.com/QMCHE/diary-server/utils"
+	"github.com/gin-gonic/gin"
+)
+
+// VerifyToken is middleware for token validation
+func VerifyToken() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		tokenString, err := c.Request.Cookie("access-token")
+		if err != nil || strings.Trim(tokenString.Value, " ") == "" {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": "Token is required",
+			})
+			c.Abort()
+			return
+		}
+
+		if utils.IsExpired(tokenString.Value) {
+			c.JSON(http.StatusUnauthorized, gin.H{
+				"error": "Token is expired",
+			})
+			c.Abort()
+			return
+		}
+
+		claims, err := utils.VerifyToken(tokenString.Value)
+		if err != nil {
+			log.Print(err)
+			c.JSON(http.StatusUnauthorized, gin.H{
+				"error": "Invalid token",
+			})
+			c.Abort()
+			return
+		}
+
+		c.Set("claims", claims)
+		c.Next()
+	}
+}
